74f38e82d22945dcdf20cf23999e8ede478b4d2c
Christopher <Christopher@DESKTOP-ECOF7C6>
2020-03-02 13:56:02 +0100

    Fixade index, la till range för tostringen i userklassen, la till winner i racingtrack, som visar vinnaren när antal clicks kommer upp till user click range

--- src/java/model/User.java
+++ src/java/model/User.java
@@ -10,17 +10,21 @@ package model;
  * @author Christopher
  */
 public class User {
+
     private String name;
     private Long clicks;
-    
+
+    private Integer range = 30;
+
     public User() {
         clicks = 0L;
     }
+
     public User(String name, Long clicks) {
         this.name = name;
         this.clicks = clicks;
     }
-    
+
     public void click() {
         clicks++;
     }
@@ -40,7 +44,15 @@ public class User {
     public void setClics(Long clics) {
         this.clicks = clics;
     }
-    
+
+    public Integer getRange() {
+        return range;
+    }
+
+    public void setRange(Integer range) {
+        this.range = range;
+    }
+
     @Override
     public String toString() {
         String retur = "";
